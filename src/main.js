b58355054e8ac4daa00f0a6f31f052f3d878ee52
Théo <theokennel@gmail.com>
2023-02-01 10:20:40 +0100

    Page JS : Update Total,  Remove Items From Cart, Making fonction ready, Cart Working JS

--- src/main.js
+++ src/main.js
@@ -7,4 +7,46 @@ cartIcon.onclick = () => {
     cart.classList.add("active");
 };
 // Close Cart
-closeCart.onclick = () => cart.classList.remove("active");
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+// Cart Working JS
+if (document.readyState === 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+}
+// Making fonction ready
+function ready(){
+//     Remove Item From Cart
+    let reomveCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(reomveCartButtons)
+    for (let i = 0; i < reomveCartButtons.length; i++){
+        const button = reomveCartButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+}
+
+// Remove Items From Cart
+function removeCartItem(event){
+    const buttonClicked = event.target
+    buttonClicked.parentElement.remove()
+    updateTotal();
+}
+
+// Update Total
+function updateTotal(){
+    const cartContent = document.getElementsByClassName('cart-content')[0];
+    const cartBoxes = cartContent.getElementsByClassName('cart-box');
+    let total = 0;
+    for (let i = 0; i < reomveCartButtons.length; i++){
+        const cartBox = cartBoxes[i]
+        const priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        let price = parseFloat(priceElement.innerText.replace('$', ''));
+        let quantity = quantityElement.value;
+        total= total + (price * quantity);
+
+        document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+    }
+}
